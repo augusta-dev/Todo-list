bf899369aee6e5c2a5038927b9ec7dd3c5ff4ccc
augustan-dev <augustan.dev@gmail.com>
2023-07-14 14:32:42 +0300

    Update

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,9 @@
+import { Typography } from "@material-tailwind/react";
 const Sidebar = () => {
     return(
-        <div className="w-1/4 bg-cyan-100 h-screen"></div>
+        <div className="w-1/4 lg:w-1/6 bg-cyan-100 h-screen relative ">
+            <Typography variant="h5" className=''>Material Tailwind</Typography>
+        </div>
     )
 }
 export default Sidebar;
