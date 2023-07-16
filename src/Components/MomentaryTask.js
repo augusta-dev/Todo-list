248fafc5b8450b4772926bb2e66fe31ec15e99ab
augustan-dev <augustan.dev@gmail.com>
2023-07-16 21:01:54 +0300

    Update

--- src/Components/MomentaryTask.js
+++ src/Components/MomentaryTask.js
@@ -1,23 +1,32 @@
 import { Checkbox } from "@material-tailwind/react";
-import broom from '../Assets/broom.png';
+import broom from "../Assets/broom.png";
 
 const MomentaryTask = () => {
-    return (
-        <div>
-            <div className="flex flex-wrap -ml-2">
-            <Checkbox className="w-6 h-6 bg-transparent border-2 border-black relative p-0"/>
-            {/* <input type="checkbox" name="" id="" className="" /> */}
-            <h2 className="font-cormorant text-2xl font-semibold -ml-2 pt-2">Right Now</h2>
-            
-            </div>
-            <div className="ml-1 flex flex-wrap">
-            <img src={broom} alt="" srcset="" className="h-5 w-5"/>
-            <div className="pl-2">
-                <p>Clean the gutters</p>
-                <p><span>12:00</span> - <span>17:00</span></p>
-            </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div>
+			<div className="flex flex-wrap -ml-2">
+				<Checkbox className="w-6 h-6 bg-transparent border-2 border-black relative p-0" />
+				{/* <input type="checkbox" name="" id="" className="" /> */}
+				<h2 className="font-cormorant text-2xl font-semibold -ml-2 pt-2">
+					Right Now
+				</h2>
+			</div>
+			<div className="ml-1 flex flex-wrap">
+				<img
+					src={broom}
+					alt=""
+					srcset=""
+					className="h-5 w-5"
+				/>
+				<div className="pl-2 -mt-1 leading-5">
+					<p>Clean the gutters</p>
+					<p>
+						<span className="text-yellow-900">12:00</span> -{" "}
+						<span className="text-green-900">17:00</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 export default MomentaryTask;
