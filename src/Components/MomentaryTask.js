f66b612b0a2455eb779f5fd137ab75859b135420
augustan-dev <augustan.dev@gmail.com>
2023-07-16 20:56:53 +0300

    Update

--- src/Components/MomentaryTask.js
+++ src/Components/MomentaryTask.js
@@ -4,17 +4,17 @@ import broom from '../Assets/broom.png';
 const MomentaryTask = () => {
     return (
         <div>
-            <div className="flex flex-wrap -ml-4">
+            <div className="flex flex-wrap -ml-2">
             <Checkbox className="w-6 h-6 bg-transparent border-2 border-black relative p-0"/>
             {/* <input type="checkbox" name="" id="" className="" /> */}
             <h2 className="font-cormorant text-2xl font-semibold -ml-2 pt-2">Right Now</h2>
             
             </div>
-            <div>
+            <div className="ml-1 flex flex-wrap">
             <img src={broom} alt="" srcset="" className="h-5 w-5"/>
-            <div>
+            <div className="pl-2">
                 <p>Clean the gutters</p>
-                <p>12:00 - 17:00</p>
+                <p><span>12:00</span> - <span>17:00</span></p>
             </div>
             </div>
         </div>
